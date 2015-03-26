85869f1500a59946a5bef28fbb97c9ac5be86970
Shen Yangang <younger.x.shen@gmail.com>
2015-03-26 17:14:36 +0800

    add bootstrap and jquery

--- django_super_cms/urls.py
+++ django_super_cms/urls.py
@@ -5,14 +5,23 @@
 
 from django.conf.urls import patterns, include, url
 
+# main views
 urlpatterns = patterns('django_super_cms.views.main',
                        url(r'^$', 'index', name='')
                        )
 
+# account views
 # urlpatterns += patterns('django_super_cms.views.account',
 #                         url(r'ss', '', name='')
 #                         ),
 
+# post views
 urlpatterns += patterns('django_super_cms.views.post',
                         url(r'post/(?P<post_id>[0-9]+).html$', 'post_show_view', name='dsc_post_show_view')
+                        )
+
+# admin views
+urlpatterns += patterns('django_super_cms.views.admin',
+                        url(r'admin/index.html$', 'admin_index_view', name='dsc_admin_index_view'),
+                        url(r'admin/login.html$', 'admin_login_view', name='dsc_admin_login_view')
                         )
--- django_super_cms/views/admin.py
+++ django_super_cms/views/admin.py
@@ -2,3 +2,19 @@
 # PROJECT_NAME : django-super-cms
 # FILE_NAME    : 
 # AUTHOR       : younger shen
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_safe
+from ..models import Post
+
+
+@login_required
+def admin_index_view(request):
+    return HttpResponse('admin page')
+
+
+@require_safe
+def admin_login_view(request):
+    ret = {}
+    return render(request, 'admin-ext/login.html', ret)
