bd9d071222e9a7f57edadfdca1f46d47d8702bb4
Shen Yangang <younger.x.shen@gmail.com>
2015-03-26 16:54:37 +0800

    change templates/admin to templates/dsc-admin

--- django_super_cms/views/post.py
+++ django_super_cms/views/post.py
@@ -3,7 +3,7 @@
 # FILE_NAME    : 
 # AUTHOR       : younger shen
 from django.http import HttpResponse, Http404
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_safe
 from ..models import Post
 
@@ -14,9 +14,9 @@ def index(requesst):
 
 @require_safe
 def post_show_view(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    tpl_name = post.template_name
-    return HttpResponse('i test')
+    # post = get_object_or_404(Post, id=post_id)
+    # tpl_name = post.template_name
+    return render(request, 'index.html', {})
 
 
 @require_safe
